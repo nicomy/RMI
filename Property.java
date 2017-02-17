6503b62e9722e350f35c992f1f7509b5ca412338
nicomy <nicomy_68@hotmail.fr>
2017-02-17 10:47:24 +0100

    boujour

--- Property.java
+++ Property.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 /**
  * @author morat 
  */
+
 public class Property implements IProperty, Serializable{
 	private static final long serialVersionUID = 1L;
 	private String value;
